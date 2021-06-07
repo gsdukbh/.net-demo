2303885f9e1df5f86439b80f1c35803a0334ae9c
JiaWei Lee <gsdukbh@gmail.com>
2021-06-07 14:45:16 +0800

    first commit

---
+++ eclipse-mosquitto-demo/NormalPublisher/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+namespace NormalPublisher
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string content = "hello mqtt";
+            string topic = "test";
+            string host = "127.0.0.1";
+            // 实例化Mqtt客户端
+            MqttClient client = new MqttClient(host);
+
+            string clientId = Guid.NewGuid().ToString();
+            client.Connect(clientId);
+
+            string strValue = Convert.ToString(content);
+
+            // 发布消息主题 "/home/temperature" ，消息质量 QoS 2 
+            client.Publish(topic, Encoding.UTF8.GetBytes(strValue), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, false);
+            Console.WriteLine(string.Format("publisher,topic:{0},content:{1}", topic, content));
+        }
+    }
+}
---
+++ eclipse-mosquitto-demo/mq-demo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+using uPLibrary.Networking.M2Mqtt;
+
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+
+namespace mqdemo
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");NormalPublisher
+            string topic = "test";
+            string host = "127.0.0.1";
+
+            MqttClient mqttClient = new MqttClient(host);
+
+
+            mqttClient.MqttMsgPublishReceived += client_MqttMsgPublishReceived;
+            string clientId = Guid.NewGuid().ToString();
+            mqttClient.Connect(clientId);
+
+            // 订阅主题 "/home/temperature"， 订阅质量 QoS 2 
+            mqttClient.Subscribe(new string[] { topic }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
+
+
+
+        }
+
+
+        static void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+        {
+            // 打印订阅的发布端消息
+            Console.WriteLine(string.Format("subscriber,topic:{0},content:{1}",
+                e.Topic, Encoding.UTF8.GetString(e.Message)));
+        }
+    }
+
+}
